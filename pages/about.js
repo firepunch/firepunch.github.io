04a2b106fa4515991712df9f8e112985d5cff121
firepunch <firepunch119@gmail.com>
2023-09-28 22:15:32 -0700

    Add about page

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import { css } from '@emotion/css'
+
+export default function Project() {
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+const projectStyles = {
+  recommend: css({
+  })
+}
