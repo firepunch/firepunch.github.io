1e1f85980531faa2ccabafa31a5cc9a31a17be3c
firepunch <firepunch119@gmail.com>
2023-09-30 21:16:21 -0700

    Render markdown file as content

---
+++ _articles/performance.md
@@ -0,0 +1,13 @@
+---
+title: Performance
+slug: performance
+date: '2023-09-30'
+---
+
+## Background
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ex dolore quos libero dolorum non nisi ullam provident recusandae, et dolorem laboriosam quasi maxime. Velit sint eligendi explicabo sit quibusdam.
+
+## Feature
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ex dolore quos libero dolorum non nisi ullam provident recusandae, et dolorem laboriosam quasi maxime. Velit sint eligendi explicabo sit quibusdam.
--- pages/[slug].js
+++ pages/[slug].js
@@ -2,12 +2,18 @@ import React from 'react'
 import { css } from '@emotion/css'
 import { fontStyles } from "../shared/variables"
 import ProjectCard from '../app/components/ProjectCard'
+import { remark } from 'remark'
+import html from 'remark-html'
+import matter from 'gray-matter'
+import fs from 'fs'
+import path from 'path'
 
-export default function Project() {
+export default function Project({ postData }) {
   return (
     <>
       <section className="content">
-        <h2>Project Name</h2>
+        {postData.slug}
+        <h2>{postData.title}</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
         <figure>
@@ -33,6 +39,44 @@ export default function Project() {
       </section>
     </>
   )
+}
+
+const POSTS_PATH = path.join(process.cwd(), '_articles');
+
+export async function getPostData(slug) {
+  const fullPath = path.join(POSTS_PATH, `${slug}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const matterResult = matter(fileContents)
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  return {
+    slug,
+    contentHtml,
+    ...matterResult.data,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.slug)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export const getStaticPaths = async () => {
+  const paths = fs.readdirSync(POSTS_PATH)
+    .map(path => path.replace(/\.md/, ''))
+    .map(slug => ({ params: { slug } }))
+
+  return {
+    paths,
+    fallback: false
+  }
 }
 
 const projectStyles = {
