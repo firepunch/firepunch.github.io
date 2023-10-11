c4211f1b11438efb019689831508f9b702050030
firepunch <firepunch119@gmail.com>
2023-10-11 15:30:45 -0700

    Styling home page

--- pages/_Hero.js
+++ pages/_Hero.js
@@ -1,5 +1,6 @@
 import { css } from '@emotion/css'
 import { fontStyles, spacing } from "../shared/variables"
+import EmailButton from '../components/Contact/EmailButton'
 
 export default function Hero() {
   return (
@@ -10,6 +11,7 @@ export default function Hero() {
       <h2 className={heroStyles.subTitle}>
         I help simplify complex problems and organize them into processes to improve efficiency.
       </h2>
+      <EmailButton />
     </section>
   )
 }
@@ -17,14 +19,15 @@ export default function Hero() {
 const heroStyles = {
   hero: css({
     paddingTop: spacing.topPadding,
-    marginBottom: '6rem'
+    marginBottom: '8rem'
   }),
   title: css({
-    fontSize: '3.4rem',
+    fontSize: '2.6rem',
     marginBottom: '1.2rem'
   }),
   subTitle: css({
-    lineHeight: '1.4',
-    ...fontStyles.regular
+    ...fontStyles.regular,
+    lineHeight: 1.4,
+    marginBottom: '2.4rem'
   })
 }
--- pages/_Projects.js
+++ pages/_Projects.js
@@ -1,15 +1,54 @@
-import projectSummary from '../_projects/summary.json'
+import { css } from '@emotion/css'
+import Image from 'next/image'
+
+import { colors, flexStyles, wavyLink } from '../shared/variables'
+import data from '../_projects/summary.json'
 import ProjectCard from '../components/ProjectCard'
 
 export default function Projects() {
   return (
     <section>
-      {projectSummary?.map(project => project.featured && (
+      {data?.map(project => project.featured && (
         <ProjectCard
           key={project.slug}
           {...project}
         />
       ))}
+
+      <article className={workStyles.card}>
+        <Image src="/images/madup_logo.svg" alt="Madup Logo" className={workStyles.logo} width={110} height={130} />
+        <h3 className={workStyles.title}>Madup</h3>
+        <p className={workStyles.description}>
+          Early on at <a href="https://madup.com/en.html" target="_blank">Madup</a> I built main products for both internal and <a href="https://lever.me" target="_blank">external</a> users and <a href="https://tech.madup.com/">blog</a>.
+        </p>
+      </article>
     </section>
   )
+}
+
+const workStyles = {
+  card: css({
+    position: 'relative',
+    background: colors.lightBlue,
+    fontSize: '1.6rem',
+    padding: '3rem 2rem',
+    borderRadius: '1rem',
+    a: wavyLink
+  }),
+  logo: css({
+    position: 'absolute',
+    right: '2rem',
+    bottom: 0,
+    opacity: 0.2,
+  }),
+  title: css({
+    fontSize: '2.2rem',
+    marginBottom: '1rem',
+  }),
+  description: css({
+    position: 'relative',
+    lineHeight: 1.4,
+    width: '80%',
+    zIndex: 2,
+  })
 }
