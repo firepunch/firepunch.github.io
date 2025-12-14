57c1b745960b3429edab1d9bba5872cda567f9f0
Mirim Yu <firepunch119@gmail.com>
2025-12-14 10:16:14 -0500

    remove project section

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,12 @@ import { css } from '@emotion/css'
 
 import { spacing, mq } from '../shared/variables'
 import Hero from './_Hero'
-import Projects from './_Projects'
 import Contact from '../components/Contact'
 
 export default function Home() {
   return (
     <section className={homeStyles.home}>
       <Hero />
-      <Projects />
       <Contact />
     </section>
   )
